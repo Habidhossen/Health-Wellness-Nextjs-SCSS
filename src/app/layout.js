902997d90a796dda924010f245dd2a13da48ddbe
Habidhossen <habidhossen2@gmail.com>
2024-11-19 17:21:41 +0600

    initial setup done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,8 @@
-import localFont from "next/font/local";
-import "./globals.css";
+import { Onest } from "next/font/google";
+import "../styles/globals.scss";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// Initialize the Onest font
+const onest = Onest({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={onest.className}>{children}</body>
     </html>
   );
 }
